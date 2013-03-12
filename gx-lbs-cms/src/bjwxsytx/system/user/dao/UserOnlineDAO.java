6b6d010c11a82b94d149ffda9416949dce2d3577
boostwade <boostwade@gmail.com>
2013-03-12 07:03:21 +0000

    当一个账号登录后，若还有人用这个账号登录，先登录的人再访问页面，会提示：您的账号已经在其他地方登录。

---
+++ gx-lbs-cms/src/bjwxsytx/system/user/dao/UserOnlineDAO.java
@@ -0,0 +1,17 @@
+package bjwxsytx.system.user.dao;
+
+import bjwxsytx.common.CommonDAO;
+
+import bjwxsytx.system.entity.TUserOnline;
+
+
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class UserOnlineDAO extends CommonDAO<TUserOnline>{
+
+	
+}
